4de1e983161c75d345fab3b5675b2296b39bf8a4
andreivercosa <andreivercosa@gmail.com>
2019-02-26 17:48:26 -0400

    v1

---
+++ Contante/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+
+namespace Contante
+{
+    public partial class MainPage : ContentPage
+    {
+        int i = 1;
+        public MainPage()
+        {
+           
+            InitializeComponent();
+
+        }
+
+        void ClicadaDoBotao(object sender, EventArgs e)
+        {
+            if (i % 2 != 0) { 
+                label_status.Text = "VOCE CLICOU";
+                button_meClica.Text = "CLICADO";
+            }
+            if (i % 2 == 0)
+            {
+                label_status.Text = "VAMOS NESSA";
+                button_meClica.Text = "ME CLICA";
+            }
+            i++;
+        }
+
+    }
+}
